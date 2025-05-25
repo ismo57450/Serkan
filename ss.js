8a4bb262a3db035a2c0fb474357938bb714b38b3
ismo57450 <111062566+ismo57450@users.noreply.github.com>
2025-05-25 21:26:21 +0200

    Add files via upload

---
+++ ss.js
@@ -0,0 +1,33 @@
+var sidenav = document.getElementById("mySidenav");
+var openBtn = document.getElementById("openBtn");
+var closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  sidenav.classList.add("active");
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  sidenav.classList.remove("active");
+}
+
+const slide = ["IMG_0037.png","IMG_0042.png"];
+let numero = 0;
+
+function ChangeSlide(sens) {
+    numero = numero + sens;
+    if (numero < 0)
+        numero = slide.length - 1;
+    if (numero > slide.length - 1)
+        numero = 0;
+    document.getElementById("slide").src = slide[numero];
+}
+
+  function flipCard(container) {
+    const card = container.querySelector('.card');
+    card.classList.toggle('flipped');
+  }
